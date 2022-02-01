c0e2b0aba478094ebde47bf419a5af4f6de7495a
rich11berr <rinocraft.tv@gmail.com>
2022-02-01 16:01:14 +0400

    init: init dropdown

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,7 @@
 const inputSec = document.getElementById("input-section");
 const buttonSec = document.getElementById("button-section");
+const dropdownSec = document.getElementById("dropdown_section");
+
 const descr = document.querySelector(".description");
 
 const btns = document.querySelectorAll(".nav__btn");
@@ -10,10 +12,17 @@ btns.forEach((e) => {
         if (inputSec.id === e.dataset.section) {
             inputSec.style.display = "block";
             buttonSec.style.display = "none";
+            dropdownSec.style.display = "none";
         }
         if (buttonSec.id === e.dataset.section) {
             buttonSec.style.display = "block";
             inputSec.style.display = "none";
+            dropdownSec.style.display = "none";
+        }
+        if (dropdownSec.id === e.dataset.section) {
+            dropdownSec.style.display = "block";
+            inputSec.style.display = "none";
+            buttonSec.style.display = "none";
         }
     });
 });
