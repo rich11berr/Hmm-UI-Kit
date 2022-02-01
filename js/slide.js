dcf8684368ae361be0211bba0970e51d4d53012e
rich11berr <rinocraft.tv@gmail.com>
2022-02-01 16:29:22 +0400

    fix: fix slide.js

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,4 @@
-const inputSec = document.getElementById("input-section");
-const buttonSec = document.getElementById("button-section");
-const dropdownSec = document.getElementById("dropdown_section");
+const sections = document.querySelectorAll(".container");
 
 const descr = document.querySelector(".description");
 
@@ -9,20 +7,12 @@ const btns = document.querySelectorAll(".nav__btn");
 btns.forEach((e) => {
     e.addEventListener("click", () => {
         descr.style.display = "none";
-        if (inputSec.id === e.dataset.section) {
-            inputSec.style.display = "block";
-            buttonSec.style.display = "none";
-            dropdownSec.style.display = "none";
-        }
-        if (buttonSec.id === e.dataset.section) {
-            buttonSec.style.display = "block";
-            inputSec.style.display = "none";
-            dropdownSec.style.display = "none";
-        }
-        if (dropdownSec.id === e.dataset.section) {
-            dropdownSec.style.display = "block";
-            inputSec.style.display = "none";
-            buttonSec.style.display = "none";
+        for (let i = 0; i < sections.length; ++i) {
+            if (sections[i].id === e.dataset.section) {
+                sections[i].style.display = "block";
+            } else {
+                sections[i].style.display = "none";
+            }
         }
     });
 });
