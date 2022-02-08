a6ccc42187a0a3c96c33e7ffe6e96a5ba4f645a4
Rinat Yagudin <79003546+rich11berr@users.noreply.github.com>
2022-02-08 16:31:04 +0400

    v1.0 Hmm-UI-kit (#13)
    feat: stable version

---
+++ js/slide.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll(".container");
+
+const descr = document.querySelector(".description");
+
+const btns = document.querySelectorAll(".nav__btn");
+
+btns.forEach((e) => {
+    e.addEventListener("click", () => {
+        descr.style.display = "none";
+        for (let i = 0; i < sections.length; ++i) {
+            if (sections[i].id === e.dataset.section) {
+                sections[i].style.display = "block";
+            } else {
+                sections[i].style.display = "none";
+            }
+        }
+    });
+});
