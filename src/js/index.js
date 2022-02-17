06498f3c203ae18758e561a72f1b493f9d0f7a96
rich11berr <rinocraft.tv@gmail.com>
2022-02-17 17:09:56 +0400

    feat: turn everything to components

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,19 @@ import "./components/button/comp_button";
 import "./components/dragDrop/comp_dragdrop";
 import "./components/dialog/comp_dialog";
 import "./components/fileUpload/comp_fileUpload";
+import "./components/checkbox/comp_checkbox";
+import "./components/tree/comp_tree";
 //scripts
 import "./components/fileUpload/fileUpload";
-import "./tree";
+import "./components/tree/tree";
 import "./components/dropdown/dropdown";
 import "./components/input/telMask";
 import "./components/dragDrop/dragDrop";
 import "./components/dialog/dialog";
 //styles
 import "../styles/main.scss";
+
+//nav
 const sections = document.querySelectorAll(".container");
 
 const descr = document.querySelector(".description");
